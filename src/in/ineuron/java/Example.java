50fd207749b59c15d02ffc9c3378589489da8d4e
Tejas Rajendra Donadkar <72977141+thdonadkar@users.noreply.github.com>
2023-02-23 11:39:10 +0530

    Third Commit

--- src/in/ineuron/java/Example.java
+++ src/in/ineuron/java/Example.java
@@ -5,6 +5,10 @@ public class Example {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		int[] arr = {1,2,3,4,5,6,7,8,9,10};
+		for(int a:arr) {
+			System.out.println(a);
+		}
 	}
 
 }
